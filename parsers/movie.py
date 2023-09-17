5ccc95f0e76e0129898868a3dd2322dac8c7259b
Carlos Loaiza <carlosdamian05329@gmail.com>
2023-09-17 16:31:13 -0500

    feat: agrega verificaciones y códigos de estado

--- parsers/movie.py
+++ parsers/movie.py
@@ -1,10 +1,24 @@
 # External libraries
+from datetime import date
 from pydantic import BaseModel
+from pydantic import Field
 
 
 class Movie(BaseModel):
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
+    title: str = Field(min_length=1, max_length=50)
+    overview: str = Field(min_length=10, max_length=100)
+    year: int = Field(ge=0, le=date.today().year)
+    rating: float = Field(ge=0, le=10.0)
+    # TODO: Agregar un enum que tenga todos las posibles categorias de películas
+    category: str = Field(min_length=1, max_length=50)
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'title': 'Título de ejemplo',
+                'overview': 'Resumen de película',
+                'year': 2017,
+                'rating': 9.5,
+                'category': 'Acción'
+            }
+        }
--- routers/movies.py
+++ routers/movies.py
@@ -2,7 +2,9 @@
 import uuid
 
 from fastapi import APIRouter
-from fastapi.responses import Response
+from fastapi.responses import JSONResponse
+from fastapi import Path
+from fastapi import Query
 from fastapi import status
 
 # Own libraries
@@ -17,45 +19,68 @@ movies_router = APIRouter(
 )
 
 
-@movies_router.get('', response_model=dict[str, Movie])
+@movies_router.get(
+    '',
+    response_model=dict[str, Movie],
+    status_code=status.HTTP_200_OK)
 def get_movies():
     return data_movies
 
 
-@movies_router.get('/{id_movie}', response_model=Movie)
+@movies_router.get(
+    '/{id_movie}',
+    response_model=Movie,
+    status_code=status.HTTP_200_OK)
 def get_movie(id_movie: str):
     if id_movie in data_movies.keys():
         return data_movies[id_movie]
     else:
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(
+            content='No se ha encontrado la película',
+            status_code=status.HTTP_404_NOT_FOUND)
 
 
-@movies_router.get('/', response_model=dict[str, Movie])
-def get_movies_by_category(category: str = 'Acción'):
+@movies_router.get(
+    '/',
+    response_model=dict[str, Movie],
+    status_code=status.HTTP_200_OK)
+def get_movies_by_category(
+        category: str = Query(default='Acción', min_length=1, max_length=50)):
     movies_data = {
         id_movie: movie for id_movie, movie in data_movies.items()
         if movie['category'] == category}
     return movies_data
 
 
-@movies_router.post('/post', response_model=dict[str, Movie])
+@movies_router.post(
+    '/post',
+    response_model=dict[str, Movie],
+    status_code=status.HTTP_201_CREATED)
 def post_new_movie(new_movie: Movie):
     new_id = str(uuid.uuid4())
     movies = {**data_movies, new_id: new_movie.model_dump()}
     return movies
 
 
-@movies_router.put('/put/{id_movie}', response_model=dict[str, Movie])
-def put_movie(id_movie: str, new_data_movie: Movie):
+@movies_router.put(
+    '/put/{id_movie}',
+    response_model=dict[str, Movie],
+    status_code=status.HTTP_200_OK)
+def put_movie(
+        new_data_movie: Movie,
+        id_movie: str = Path(min_length=36, max_length=36)):
     if id_movie not in data_movies.keys():
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND)
     data_movies[id_movie] = new_data_movie.model_dump()
     return {id_movie: data_movies[id_movie]}
 
 
-@movies_router.delete('/delete/{id_movie}', response_model=dict[str, Movie])
+@movies_router.delete(
+    '/delete/{id_movie}',
+    response_model=dict[str, Movie],
+    status_code=status.HTTP_200_OK)
 def delete_movie(id_movie: str):
     if id_movie not in data_movies.keys():
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND)
     del data_movies[id_movie]
     return data_movies
